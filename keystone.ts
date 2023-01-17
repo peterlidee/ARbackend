33ba962630c4bb58fcd848af3c0fc37ef397bffd
peterlidee <peter@lidee.be>
2023-01-17 12:08:36 +0100

    init

---
+++ keystone.ts
@@ -0,0 +1,56 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/something';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 30, // how long should they stay signed in
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // todo: add intial roles in here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // todo add data seeding here
+    },
+    lists: createSchema({
+      // schema items go in here
+      User,
+      Product,
+    }),
+    ui: {
+      // show the ui only for people who pass this test
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    // Todo add session values in here
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL query
+      User: 'id',
+    }),
+  })
+);
